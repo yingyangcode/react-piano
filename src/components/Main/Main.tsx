aacf36e1e243148dddb63b797cbeacd0874824fb
yingyangcode <marcel.anis.21@gmail.com>
2022-01-28 18:26:52 -0500

    AudioContext provider, Key, Keyboard and NoAudio componets

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,6 @@
 import { Keyboard } from "../Keyboard";
 import { NoAudioMessage } from "../NoAudioMessage";
-import { useAudioContext } from "./AudioContextProvider";
+import { useAudioContext } from "../AudioContextProvider";
 
 export const Main = () => {
   const AudioContext = useAudioContext();
