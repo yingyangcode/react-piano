4b4375151941add8f78d7d66b891c57dd2afcd1b
yingyangcode <marcel.anis.21@gmail.com>
2022-01-29 16:12:48 -0500

    Instruments List, Change Instrument

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { useInstrument } from "../../state/Instrument";
 import { useAudioContext } from "../AudioContextProvider";
 import { useSoundfont } from "../../adapters/Soundfont";
 import { useMount } from "../../utils/useMount";
@@ -5,8 +7,14 @@ import { Keyboard } from "../Keyboard";
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!;
-  const { loading, play, stop, load } = useSoundfont({ AudioContext });
-  useMount(() => load());
+  const { instrument } = useInstrument();
+  const { loading, current, play, stop, load } = useSoundfont({ AudioContext });
+
+  useEffect(() => {
+    if (!loading && instrument !== current) load(instrument);
+  }, [load, loading, current, instrument]);
+
+  //   useMount(() => load());
 
   return <Keyboard loading={loading} play={play} stop={stop} />;
 };
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,9 @@
+import { Playground } from "../Playground";
 import { KeyboardWithInstrument } from "../Keyboard";
 import { NoAudioMessage } from "../NoAudioMessage";
 import { useAudioContext } from "../AudioContextProvider";
 
 export const Main = () => {
   const AudioContext = useAudioContext();
-  return !!AudioContext ? (<KeyboardWithInstrument />) : (<NoAudioMessage />);
+  return !!AudioContext ? <Playground /> : <NoAudioMessage />;
 };
