511f4db55e8c115e71dc7d07e9d5297f401aa4a8
yingyangcode <marcel.anis.21@gmail.com>
2022-01-29 15:05:07 -0500

    added soundfont adapter, Keyboard with instrument to play a sound

--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -3,16 +3,26 @@ import clsx from "clsx";
 import { NoteType } from "../../domain/note";
 import styles from "./Key.module.css";
 
+type PressCallback = () => void;
 type KeyProps = {
   type: NoteType;
   label: string;
   disabled?: boolean;
+
+  onUp: PressCallback;
+  onDown: PressCallback;
 };
 
 export const Key: FunctionComponent<KeyProps> = (props) => {
-  const { type, label, ...rest } = props;
+  const { type, label, onDown, onUp, ...rest } = props;
   return (
-    <button className={clsx(styles.key, styles[type])} type="button" {...rest}>
+    <button
+      className={clsx(styles.key, styles[type])}
+      type="button"
+      {...rest}
+      onMouseDown={onDown}
+      onMouseUp={onUp}
+    >
       {label}
     </button>
   );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,8 @@
-import { Keyboard } from "../Keyboard";
+import { KeyboardWithInstrument } from "../Keyboard";
 import { NoAudioMessage } from "../NoAudioMessage";
 import { useAudioContext } from "../AudioContextProvider";
 
 export const Main = () => {
   const AudioContext = useAudioContext();
-  return !!AudioContext ? <Keyboard /> : <NoAudioMessage />;
+  return !!AudioContext ? (<KeyboardWithInstrument />) : (<NoAudioMessage />);
 };
